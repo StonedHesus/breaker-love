55882945e9c18c0640501d2c0b8c2f5d90ba1490
stonedhesus <ionescu_paul@outlook.com>
2022-12-03 11:52:17 +0100

    Correct the ball-paddle collision so that no matter the angle at which the collision occurs, the end result will be a geometrically sound outcome, and hence no unaccount behaviour will occur during the lifespan of the software.

--- src/states/play-state/PlayState.lua
+++ src/states/play-state/PlayState.lua
@@ -20,7 +20,17 @@ function PlayState:update(deltaTime)
     ball:update(deltaTime)
 
     if ball:collides(paddle) then
+        -- Position the ball higher than the paddle, so as to account for the case in which it might get lower than that.
+        ball.y = ball.y - ball.height/4
         ball.deltaY = -ball.deltaY 
+
+        -- Correct the angle at which the ball bounces back.
+        if ball.x < paddle.x + (paddle.width / 2) and paddle.deltaX < 0 then 
+            ball.deltaX = -50 + -8 * (paddle.x + paddle.width / 2 - ball.x)
+        elseif ball.x > paddle.x + paddle.width / 2 and paddle.deltaX > 0 then
+            ball.deltaX = 50 + 8 * math.abs(paddle.x + paddle.width / 2 - ball.x)
+        end     
+
         gSounds['paddle-hit']:play()
     end
 end
